b88e425bc8943e588705543295df7569ea65dfaa
Chris Olstrom <chris@olstrom.com>
2016-03-10 20:48:57 -0800

    Add require for in step

--- lib/concourse-resource/rubygems.rb
+++ lib/concourse-resource/rubygems.rb
@@ -1,3 +1,4 @@
 require 'concourse-resource/rubygems/check'
+require 'concourse-resource/rubygems/in'
 require 'concourse-resource/rubygems/gem_not_found'
 require 'concourse-resource/rubygems/version'
