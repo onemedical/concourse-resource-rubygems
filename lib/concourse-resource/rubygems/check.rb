62c8e00ee8aa1e525345cbd1f3f111ff7e4f504c
Chris Olstrom <chris@olstrom.com>
2016-03-10 16:14:32 -0800

    Add #versions to RubyGems::Check

--- lib/concourse-resource/rubygems/check.rb
+++ lib/concourse-resource/rubygems/check.rb
@@ -1,9 +1,22 @@
 require 'contracts'
 require 'concourse-resource/rubygems/core'
+require 'concourse-resource/rubygems/gem_not_found'
 
 module RubyGems
   # Check Step
   class Check < Fuselage::Check
     include Core
+
+    Contract None => ArrayOf[HashOf[String, String]]
+    def versions
+      Gems
+        .versions(gem)
+        .tap { |response| raise GemNotFound, response if response.is_a? String }
+        .sort_by { |version| version['created_at'] }
+        .map { |version| { 'version' => version.fetch('number') } }
+    rescue GemNotFound => message
+      STDERR.puts message
+      abort
+    end
   end
 end
